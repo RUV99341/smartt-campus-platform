4997f88ad898dbc719c42873ae3d8a019429aed1
RITESH VERMA <158120093+RUV99341@users.noreply.github.com>
2026-01-04 13:56:14 +0530

    Update Firebase configuration details
    Updated Firebase configuration with new API key and project ID.

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { getFunctions } from "firebase/functions";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBVQwBwdFoa************************",
+  apiKey: "AIzaSyBVQwBwdFoapoGIGhgSB6qzDOyeap_GN40",
   authDomain: "smartcampusplatform-ce718.firebaseapp.com",
-  projectId: "smartcampusplatform-*****",
+  projectId: "smartcampusplatform-ce718",
   storageBucket: "smartcampusplatform-ce718.appspot.com",
-  messagingSenderId: "688341377*******",
-  appId: "1:688341377206:web:8f294c85364**********"
+  messagingSenderId: "688341377206",
+  appId: "1:688341377206:web:8f294c8536416978e6c617"
 };
 
 // Initialize Firebase
